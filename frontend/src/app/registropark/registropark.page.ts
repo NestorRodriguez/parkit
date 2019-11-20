f125de46eb198c5ff5042931d00b4f957059cca7
SaraCA77 <saritacamachoa77@gmail.com>
2019-11-20 14:30:20 -0500

    ts abrir pdf

--- frontend/src/app/registropark/registropark.page.ts
+++ frontend/src/app/registropark/registropark.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, PlatformRef } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+import { DocumentViewer, DocumentViewerOptions } from '@ionic-native/document-viewer/ngx';
+import { File } from '@ionic-native/file/ngx';
+import { FileTransfer } from '@ionic-native/file-transfer/ngx';
 
 @Component({
   selector: 'app-registropark',
@@ -7,7 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroparkPage implements OnInit {
 
-  constructor() { }
+  constructor(private platform: PlatformRef, private file: File, private ft: FileTransfer,
+    private fileOpener: FileOpener, private document: DocumentViewer) {
+
+    }
+    openLocalPdf(){
+      let filePath = this.file.applicationDirectory + 'assets';
+
+      if (this.platform.is('andorid')){
+        let fakeName = Date.now();
+        this.file.copyFile(filePath, 'TyC_PARKIT.pdf', this.file.dataDirectory, `${fakeName}.pdf`).then(result => {
+
+        });
+      }else{
+        const options: DocumentViewerOptions = {
+          title: 'Terminos Y Condiciones PARKIT'
+        }
+        this.document.viewDocument(`${filePath}/TyC_PARKIT.pdf`, 'application/pdf', options);
+      }
+    }
 
   ngOnInit() {
   }
